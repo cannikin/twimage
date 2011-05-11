2d112a8b85b7feff6b506d3ccaa44414366bb97e
Rob Cameron <cannikinn@gmail.com>
2011-05-11 09:49:07 -0400

    Change @image to @tempfile, built 0.0.5

--- lib/twimage/base.rb
+++ lib/twimage/base.rb
@@ -1,14 +1,14 @@
 module Twimage
   class Base
     
-    attr_accessor :image, :image_url, :service_url
+    attr_accessor :tempfile, :image_url, :service_url
     
     def initialize(service_url)
       response = HTTParty.get(@image_url)
       puts response.code
       if response.code == 200
-        @image = Tempfile.new(['twimage','.jpg'])
-        @image << response.body.force_encoding('utf-8')
+        @tempfile = Tempfile.new(['twimage','.jpg'])
+        @tempfile << response.body.force_encoding('utf-8')
       else
         raise ImageURLInvalid, "The image_url #{@image_url} was not found (returned a 404)"
       end
